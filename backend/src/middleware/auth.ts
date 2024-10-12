4bb506a1f57b5311fb5a0826d6f09ebd8080a9e6
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-10-12 20:19:30 +0530

    - updated Request interface to add an object 'user' which contains a id which needs to be passed as a parameter to the subsquient child routes - By declaring this as global, you're telling TypeScript that every time it encounters an Express.Request object, it will now have the user property available to it across your entire application.

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,11 +1,35 @@
+import { NextFunction, Request, Response } from 'express';
 import * as dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+
 dotenv.config();
+const JWT_SECRET = process.env.JWT_SECRET;
 
-export function auth(req : any, res: any, next: any) {
+type User = {
+    id: string | number
+}
+declare global {
+    namespace Express {
+        interface Request {
+            user?: User
+        }
+    }
+}
 
+export function auth(req: Request, res: Response, next: NextFunction) {
     const token = req.header("Authorization")?.split(' ')[1];
 
-    console.log('token', token)
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const validToken = jwt.verify(token, JWT_SECRET as string) as ({userId : string});
+    console.log('vai', validToken)
+    //'as string' is called as Type Assertion
+
+    //NOTE: Type Assertion: Using as string tells TypeScript, "Hey, I know this value will definitely be a string, so treat it as one."
+
+    req.user = { id: validToken.userId }; 
 
     next();
 }
