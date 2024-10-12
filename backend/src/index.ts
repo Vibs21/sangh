806a18fdee1f2c7237f562486e22b2d96b8edb83
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-10-12 20:32:19 +0530

    global error handler added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,27 @@
+
 import express from "express";
 import * as dotenv from 'dotenv';
-import { PrismaClient } from '@prisma/client';
 import cors from 'cors';
 
 import userRoutes from './routes/userRoutes';
 import communityRoutes from './routes/communityRoutes';
+import { auth } from './middleware/auth';
+import errorHandler from './middleware/errorHandler';
 
 dotenv.config();
 
-const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(errorHandler);
 
-app.use('/api/', userRoutes);
+app.get('/', (req, res)=> {
+    console.log('req', req.user)
+    res.send({message: req.user})
+})
+
+app.use('/api/user/', userRoutes);
 app.use('/api/', communityRoutes);
 
 
