17322d25b52335f8e97f35317c6ba53e65f2c962
Vaibhav Bajpayee <vaibhavbajpayee2109@gmail.com>
2024-10-18 23:25:14 +0530

    - added communityId in the JWT token to keep in handy

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -5,8 +5,9 @@ import jwt from 'jsonwebtoken';
 dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
-type User = {
-    id: string | number
+export type User = {
+    id: string | number,
+    communityId: string
 }
 declare global {
     namespace Express {
@@ -16,20 +17,27 @@ declare global {
     }
 }
 
+//NOTE: As this is fucntion which we are using as a middleware, typescript does not able to infer it,s type and hence we
+// explicity need to tell the type of all the req, res, next to the typescript, however it was not needed in case of
+// router, as TS knows it a router and they know what is req, res, here it's just a random word with no meaning, by giving the
+// type we are actully giving the purpose or defining or telling about the power now it posses by giving the type. 
 export function auth(req: Request, res: Response, next: NextFunction) {
+    console.log('auth middleware');
     const token = req.header("Authorization")?.split(' ')[1];
 
     if (!token) {
         return res.status(401).json({ message: 'Unauthorized' });
     }
 
-    const validToken = jwt.verify(token, JWT_SECRET as string) as ({userId : string});
+    const validToken = jwt.verify(token, JWT_SECRET as string) as ({userId : string, communityId: string});
     console.log('vai', validToken)
     //'as string' is called as Type Assertion
 
     //NOTE: Type Assertion: Using as string tells TypeScript, "Hey, I know this value will definitely be a string, so treat it as one."
 
-    req.user = { id: validToken.userId }; 
+    req.user = { id: validToken.userId, communityId: validToken.communityId }; 
+
+    //TODO: Find user by id in the DB to check if it's present or not
 
     console.log('logged in user is: ', validToken.userId)
 
